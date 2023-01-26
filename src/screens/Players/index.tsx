79711ea09c367bcd2e4afaa6a2489c74248c448c
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-01-26 20:17:32 -0300

    add Filter component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Hightlight";
 import { Input } from "@components/Input";
@@ -19,6 +20,8 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter title="time a"/>
     </Container>
   );
 }
