507f3ca3af6915ccc961b37f20bec2e2ae9e54e2
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-01-25 22:35:20 -0300

    add Highligh component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,16 @@
-import { Container } from './styles';
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Hightlight";
+
+import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
+      <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
     </Container>
   );
 }
