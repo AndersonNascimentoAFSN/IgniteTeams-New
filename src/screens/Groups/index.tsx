170f927962d0e95dc50272216a6186a2698c8b54
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-01 12:05:44 -0300

    add type routes

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Hightlight";
 import { GroupCard } from "@components/GroupCard";
