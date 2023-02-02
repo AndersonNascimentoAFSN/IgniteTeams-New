fcf0f080e3210d805571b26bbfd3b4cf5bc3cd71
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-02 14:33:18 -0300

    add remove Async Storage

--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { COLLECTION } from "./storageConfig";
 
+// import { COLLECTION } from "./storageConfig";
 // type Item = valueof typeof COLLECTION;
 // type Item = Partial<Record<COLLECTION, any>>;
 
@@ -23,6 +23,14 @@ export class Storage {
   static async setItem(item: string, value: string): Promise<void> {
     try {
       await AsyncStorage.setItem(item, value);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async removeItem(item: string): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(item);
     } catch (error) {
       throw error;
     }
