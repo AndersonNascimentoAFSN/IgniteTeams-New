22d7fb6788bd6a5ccd7ff10e674fa74901a61510
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-02 14:38:28 -0300

    criando uma classe de errors personalizada

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,9 +1,18 @@
 import { COLLECTION } from "@storage/storageConfig";
 import { Storage } from "@storage/storage";
+import { AppError } from "@utils/AppError";
 
 export async function groupCreate(newGroup: string) {
   try {
     const storedGroups = await Storage.getItem(COLLECTION.GROUP);
+
+    const groupAlreadyExists = storedGroups.includes(newGroup);
+
+    if (groupAlreadyExists) {
+      throw new AppError(
+        `Já existe um grupo cadastrado com o nome ${newGroup}.`
+      );
+    }
 
     const storage = JSON.stringify([...storedGroups, newGroup]);
 
